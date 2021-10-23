bfd50d346b3fda92fc73d7896e5fe1ba46000619
jaenek <jaenek@protonmail.com>
2021-10-24 01:11:22 +0200

    Rename, add useful methods

--- frame_ctrl.py
+++ frame_ctrl.py
@@ -11,35 +11,35 @@ VENDOR_ID= 0x04e8
 MODELS = {
     'SPF-72H': {
         'product_id': (0x200a, 0x200b),
-        'resolution': '800x480',
+        'geometry': '800x480',
     },
     'SPF-75H': {
         'product_id': (0x200e, 0x200f),
-        'resolution': '800x480',
+        'geometry': '800x480',
     },
     'SPF-83H': {
         'product_id': (0x200c, 0x200d),
-        'resolution': '800x480',
+        'geometry': '800x480',
     },
     'SPF-85H': {
         'product_id': (0x2012, 0x2013),
-        'resolution': '800x480',
+        'geometry': '800x480',
     },
     'SPF-87H': {
         'product_id': (0x2033, 0x2034),
-        'resolution': '800x480',
+        'geometry': '800x480',
     },
     'SPF-87H (old firmware)': {
         'product_id': (0x2025, 0x2026),
-        'resolution': '800x480',
+        'geometry': '800x480',
     },
     'SPF-107H': {
         'product_id': (0x2035, 0x2036),
-        'resolution': '800x480',
+        'geometry': '800x480',
     },
     'SPF-107H (old firmware)': {
         'product_id': (0x2027, 0x2028),
-        'resolution': '800x480',
+        'geometry': '800x480',
     },
 }
 
@@ -109,7 +109,6 @@ class FrameController():
 #        result = self.dev.ctrl_transfer(CTRL_TYPE_STANDARD | CTRL_IN | CTRL_RECIPIENT_DEVICE, 0x06, 0x0300, 0x00, 255)
 #        expect(result, [ 0x04, 0x03, 0x09, 0x04 ])
 
-
         if self.current_model in ('SPF-75H'):
             result = self.dev.ctrl_transfer(CTRL_TYPE_STANDARD | CTRL_IN | CTRL_RECIPIENT_DEVICE, 0x06, 0x0301, 0x0409, 255)
 
@@ -124,9 +123,15 @@ class FrameController():
             self.chunky_write(buf)
             pos += BUFFER_SIZE
 
-
 #        result = self.dev.ctrl_transfer(CTRL_TYPE_VENDOR | CTRL_IN | CTRL_RECIPIENT_DEVICE, 0x06, 0x00, 0x00, 2)
 #        expect(result, [ 0x00, 0x00 ])
+
+    def write_image_from_file(self, filename):
+        with open(filename, "rb") as f:
+            self.write_image(f.read())
+
+    def get_display_geometry(self):
+        return MODELS[self.current_model]['geometry']
 
 if __name__ == '__main__':
     frame_ctrl = FrameController()
@@ -134,10 +139,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 2 or sys.argv[1] == "-":
         print("Reading file from stdin")
         content = sys.stdin.buffer.read()
+        frame_ctrl.write_image(content)
     else:
         print("Reading from file " + sys.argv[1])
-        f = open(sys.argv[1], "rb")
-        content = f.read()
-        f.close()
-
-    frame_ctrl.write_image(content)
+        frame_ctrl.write_image_from_file(sys.argv[1])
